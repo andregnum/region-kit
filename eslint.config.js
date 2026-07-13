30f3986ab64571236e28ca355cdbafd26d3764c9
andretoron <andregnum@gmail.com>
2026-07-13 21:28:26 +0700

    build: configure Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,27 @@
-import js from '@eslint/js'
-import { defineConfig } from 'eslint/config'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig([
   {
-    name: 'region-kit/ignores',
-    ignores: ['**/node_modules/**', '**/dist/**', '**/coverage/**']
+    name: "region-kit/ignores",
+    ignores: ["**/node_modules/**", "**/dist/**", "**/coverage/**"],
   },
   {
-    name: 'region-kit/javascript-and-typescript',
-    files: ['**/*.{js,cjs,mjs,ts,cts,mts}'],
-    extends: [js.configs.recommended, tseslint.configs.recommended],
+    name: "region-kit/javascript-and-typescript",
+    files: ["**/*.{js,cjs,mjs,ts,cts,mts}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      eslintConfigPrettier,
+    ],
     languageOptions: {
-      globals: globals.node
+      globals: globals.node,
     },
     linterOptions: {
-      reportUnusedDisableDirectives: 'error'
-    }
-  }
-])
+      reportUnusedDisableDirectives: "error",
+    },
+  },
+]);
